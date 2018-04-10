7a477dbfe3efadff0309339f8ff9ce4f21a7ddfc
Ben Hasselgren <ben.nadalito@gmail.com>
2018-04-10 13:41:43 +0000

    Completed byotest challenges

--- byotest.py
+++ byotest.py
@@ -1,6 +1,6 @@
-def number_of_evens(numbers):
-    return 0
-
+def test_between_limits(a, b, c):
+    assert a > b and a < c, "{0} is not between limits {1} and {2}".format(a, b, c) 
+ 
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
     
@@ -9,5 +9,9 @@ def test_not_equal(a, b):
     
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item)
-    
-test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+
+test_is_in([1,2,3,4,5], 5)
+test_not_equal(3, 2)
+test_between_limits(1, 3, 9)
+
+print("All tests passed")
